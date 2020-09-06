12816bb9ce346cc6a70774a3e2b3dab5c48ce10c
Caio Gondim <me@caiogondim.com>
2020-09-06 17:38:46 -0400

    compose now supports generators as 1st argument

--- src/compose/index.js
+++ src/compose/index.js
@@ -1,19 +1,31 @@
 const isGenerator = require('../_shared/is-generator')
 const isPromise = require('../_shared/is-promise')
+const isAsyncGeneratorFunction = require('../_shared/is-async-generator-function')
+const isGeneratorFunction = require('../_shared/is-generator-function')
 
 function validateGenerators(...gens) {
   if (!gens || gens.length < 2) {
     throw new TypeError('Must have at least 2 generators')
   }
 
-  for (let gen of gens) {
-    if (
-      typeof gen !== 'function' ||
-      gen.constructor.name !== 'AsyncGeneratorFunction'
-    ) {
-      throw new TypeError(
-        `Only AsyncGeneratorFunction is accepted. Got ${gen.constructor.name}`
-      )
+  for (let i = 0; i < gens.length; i += 1) {
+    const gen = gens[i]
+    if (i === 0) {
+      if (
+        !isAsyncGeneratorFunction(gen) &&
+        !isGeneratorFunction(gen) &&
+        !isGenerator(gen)
+      ) {
+        throw new TypeError(
+          `Only AsyncGeneratorFunction, GeneratorFunction, or generator instances are accepted as first argument.`
+        )
+      }
+    } else {
+      if (!isAsyncGeneratorFunction(gen)) {
+        throw new TypeError(
+          `Only AsyncGeneratorFunction are accepted past the first argument.`
+        )
+      }
     }
   }
 }
@@ -22,7 +34,7 @@ async function* compose(...gens) {
   validateGenerators(...gens)
 
   // Transforms foo -> bar -> qux into qux(bar(foo()))
-  let composed = gens[0]()
+  let composed = isGenerator(gens[0]) ? gens[0] : gens[0]()
   for (let i = 1; i < gens.length; i += 1) {
     const curGen = gens[i]
     composed = curGen(composed)
--- src/compose/test.js
+++ src/compose/test.js
@@ -5,6 +5,7 @@ const delay = require('../delay')
 const {
   async: createNumbersGenerator,
 } = require('../_shared/create-numbers-generator')
+const createFibonacciGenerator = require('../_shared/create-fibonacci-generator')
 
 it('composes generators', async () => {
   const composed = compose(
@@ -30,4 +31,46 @@ it('throws error if less than 2 generators are passed as argument', async () => 
   const gens = [createNumbersGenerator]
   const composed = compose(...gens)
   expect(() => composed.next()).rejects.toThrow(TypeError)
+})
+
+it('accepts a generator as first argument', async () => {
+  const composed = compose(
+    createNumbersGenerator(), // returns a generator instance
+    delay(1),
+    map((x) => x * 2),
+    take(5)
+  )
+  const output = []
+  for await (let val of composed) {
+    output.push(val)
+  }
+  expect(output).toEqual([2, 4, 6, 8, 10])
+})
+
+it('accepts an AsyncGeneratorFunction as first argument', async () => {
+  const composed = compose(
+    createNumbersGenerator,
+    delay(1),
+    map((x) => x * 2),
+    take(5)
+  )
+  const output = []
+  for await (let val of composed) {
+    output.push(val)
+  }
+  expect(output).toEqual([2, 4, 6, 8, 10])
+})
+
+it('accepts a GeneratorFunction as first argument', async () => {
+  const composed = compose(
+    createFibonacciGenerator,
+    delay(1),
+    map((x) => x * 2),
+    take(5)
+  )
+  const output = []
+  for await (let val of composed) {
+    output.push(val)
+  }
+  expect(output).toEqual([2, 2, 4, 6, 10])
 })
