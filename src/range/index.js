30a88a104659db646a3e48a3d00bb541fd0a04e4
Caio Gondim <me@caiogondim.com>
2020-09-07 08:25:39 -0400

    Add range

---
+++ src/range/index.js
@@ -0,0 +1,27 @@
+const enumerate = require('../enumerate')
+
+async function* range(begin, end, gen) {
+  if (!Number.isInteger(begin)) {
+    throw new TypeError('begin must be an integer number')
+  }
+
+  if (!Number.isInteger(end)) {
+    throw new TypeError('end must be an integer number')
+  }
+
+  if(begin > end) {
+    throw new RangeError('end must be greater than begin')
+  }
+
+  for await (let [index, value] of enumerate(gen)) {
+    if (index < begin) {
+      continue
+    }
+    if (index >= end) {
+      return
+    }
+    yield value
+  }
+}
+
+module.exports = range
