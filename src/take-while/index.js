dede12eef1fe36d4fe160e733cee668b8b206470
Caio Gondim <me@caiogondim.com>
2020-09-06 21:24:10 -0400

    Add takeWhile

---
+++ src/take-while/index.js
@@ -0,0 +1,26 @@
+async function* takeWhile(predicate, gen) {
+  for await (let value of gen) {
+    if (predicate(value)) {
+      yield value
+    } else {
+      break
+    }
+    
+  }
+}
+
+function composable(predicate) {
+  return async function* composableTakeWhile(gen) {
+    yield* takeWhile(predicate, gen)
+  }
+}
+
+function main(predicate, gen) {
+  if(!gen) {
+    return composable(predicate)
+  } else {
+    return takeWhile(predicate, gen)
+  }
+}
+
+module.exports = main
