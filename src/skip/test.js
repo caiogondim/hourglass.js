5f3d3c54fae0d7a9191d9f2fdac29dbd3c525c37
Caio Gondim <me@caiogondim.com>
2020-09-06 21:04:00 -0400

    Add skip

---
+++ src/skip/test.js
@@ -0,0 +1,17 @@
+const skip = require('.')
+const take = require('../take')
+const compose = require('../compose')
+const consume = require('../consume')
+const createFibonacciGenerator = require('../_shared/create-fibonacci-generator')
+
+it('skips first `n` values of a generator', async () => {
+  const composed = take(5, skip(3, createFibonacciGenerator()))
+  const output = await consume(composed)
+  expect(output).toEqual([3, 5, 8, 13, 21])
+})
+
+it('is composable', async () => {
+  const composed = compose(createFibonacciGenerator(), skip(3), take(5))
+  const output = await consume(composed)
+  expect(output).toEqual([3, 5, 8, 13, 21])
+})
