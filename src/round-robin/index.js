9b8d5cbed722b4a4b2d8986708b4677ccb8891d4
Caio Gondim <me@caiogondim.com>
2020-09-08 23:45:54 +0000

    Add roundRobin

--- src/round-robin/index.js
+++ src/round-robin/index.js
@@ -1,3 +1,38 @@
 function roundRobin(...gens) {
-  
+  let i = 0
+  let curGen = gens[0]
+
+  return {
+    [Symbol.asyncIterator]() {
+      return this
+    },
+    async next() {
+      while (true) {
+        const generatorOutput = await curGen[Symbol.asyncIterator]().next()
+        if (generatorOutput.done) {
+          gens = gens.filter((gen) => gen !== curGen)
+        }
+
+        if (gens.length <= 0) {
+          return {
+            done: true,
+          }
+        }
+
+        i = (i + 1) % gens.length
+        curGen = gens[i]
+
+        if (generatorOutput.done) {
+          continue
+        }
+
+        return {
+          value: generatorOutput.value,
+          done: false,
+        }
+      }
+    },
+  }
 }
+
+module.exports = roundRobin
