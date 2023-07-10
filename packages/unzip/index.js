3680a6f8812a04bc04bfdfa05a84c46a086cbd77
Caio Gondim <me@caiogondim.com>
2023-07-10 19:53:54 -0400

    fix: unzip

--- packages/unzip/index.js
+++ packages/unzip/index.js
@@ -6,43 +6,32 @@ function unzip(zipped) {
   const zippedQueue2 = []
 
   async function* unziped1() {
-    // Check if there are generated values on the queue before consuming the ziped generator.
-    while (zippedQueue1.length > 0) {
-      yield zippedQueue1.shift()
+    for await (const [value1, value2] of zipped) {
+      zippedQueue1.push(value1)
+      zippedQueue2.push(value2)
+
+      while (zippedQueue1.length > 0) {
+        yield zippedQueue1.shift()
+      }
     }
 
-    for await (const [value1, value2] of zipped) {
-      // Check if there are generated values on the queue before `yield`-ing from ziped generator.
-      while (zippedQueue1.length > 0) {
-        yield zippedQueue1.shift()
-      }
-
-      zippedQueue2.push(value2)
-      yield value1
-
-      // Check if there are generated value on the queue before iterating over the ziped generator.
-      while (zippedQueue1.length > 0) {
-        yield zippedQueue1.shift()
-      }
+    while (zippedQueue1.length > 0) {
+      yield zippedQueue1.shift()
     }
   }
 
   async function* unziped2() {
-    while (zippedQueue2.length > 0) {
-      yield zippedQueue2.shift()
+    for await (const [value1, value2] of zipped) {
+      zippedQueue1.push(value1)
+      zippedQueue2.push(value2)
+
+      while (zippedQueue2.length > 0) {
+        yield zippedQueue2.shift()
+      }
     }
 
-    for await (const [value1, value2] of zipped) {
-      while (zippedQueue2.length > 0) {
-        yield zippedQueue2.shift()
-      }
-
-      zippedQueue1.push(value1)
-      yield value2
-
-      while (zippedQueue2.length > 0) {
-        yield zippedQueue2.shift()
-      }
+    while (zippedQueue2.length > 0) {
+      yield zippedQueue2.shift()
     }
   }
 
--- packages/unzip/test.js
+++ packages/unzip/test.js
@@ -1,3 +1,4 @@
+import { sleep } from '@hourglass/sleep'
 import { map } from '../map'
 import { take } from '../take/index.js'
 import { consume } from '../consume/index.js'
@@ -47,20 +48,17 @@ it('is symmetric with `zip`', async () => {
 })
 
 it('unzips a zipped pair of async generators', async () => {
-  
-
   const zipped = zip(generateEvenNumbers(), generateOddNumbers())
   const [unzippedEvenNumbers, unzippedOddNumbers] = unzip(zipped)
 
-  const consumedUnzippedEvenNumbers = await consume(unzippedEvenNumbers)
   const consumedUnzippedOddNumbers = await consume(unzippedOddNumbers)
+  const consumedUnzippedEvenNumbers = await consume(unzippedEvenNumbers)
 
   expect(consumedUnzippedEvenNumbers).toEqual([0, 2, 4, 6, 8])
   expect(consumedUnzippedOddNumbers).toEqual([1, 3, 5, 7, 9])
 })
 
-it('returns a pair of async generators that can be consumed independently', async () => {
-
+it('returns a pair of async generators A and B, where A can be consumed first', async () => {
   const zipped = zip(generateEvenNumbers(), generateOddNumbers())
   const [unzippedEvenNumbers, unzippedOddNumbers] = unzip(zipped)
 
@@ -71,14 +69,80 @@ it('returns a pair of async generators that can be consumed independently', asyn
   expect(consumedUnzippedOddNumbers).toEqual([1, 3, 5, 7, 9])
 })
 
-it('returns a pair of async generators that can be consumed independently', async () => {
-
+it('returns a pair of async generators A and B, where B can be consumed first', async () => {
   const zipped = zip(generateEvenNumbers(), generateOddNumbers())
   const [unzippedEvenNumbers, unzippedOddNumbers] = unzip(zipped)
 
   const consumedUnzippedOddNumbers = await consume(unzippedOddNumbers)
   expect(consumedUnzippedOddNumbers).toEqual([1, 3, 5, 7, 9])
-
   const consumedUnzippedEvenNumbers = await consume(unzippedEvenNumbers)
   expect(consumedUnzippedEvenNumbers).toEqual([0, 2, 4, 6, 8])
+})
+
+it('works if unzipped generator A is being consumed faster than unzipped generator B', async () => {
+  const zipped = zip(generateEvenNumbers(), generateOddNumbers())
+  const [unzippedEvenNumbers, unzippedOddNumbers] = unzip(zipped)
+
+  const evenNumbers = []
+  const oddNumbers = []
+  await Promise.all([
+    (async () => {
+      for await (const currentEvenNumber of unzippedEvenNumbers) {
+        await sleep(1)
+        evenNumbers.push(currentEvenNumber)
+      }
+    })(),
+    (async () => {
+      await sleep(1)
+      for await (const currentOddNumber of unzippedOddNumbers) {
+        await sleep(2)
+        oddNumbers.push(currentOddNumber)
+      }
+    })(),
+  ])
+
+  expect(evenNumbers).toEqual([0, 2, 4, 6, 8])
+  expect(oddNumbers).toEqual([1, 3, 5, 7, 9])
+})
+
+it('works if unzipped generator B is being consumed faster than unzipped generator A', async () => {
+  const zipped = zip(generateEvenNumbers(), generateOddNumbers())
+  const [unzippedEvenNumbers, unzippedOddNumbers] = unzip(zipped)
+
+  const evenNumbers = []
+  const oddNumbers = []
+  await Promise.all([
+    (async () => {
+      await sleep(1)
+      for await (const currentEvenNumber of unzippedEvenNumbers) {
+        await sleep(2)
+        evenNumbers.push(currentEvenNumber)
+      }
+    })(),
+    (async () => {
+      for await (const currentOddNumber of unzippedOddNumbers) {
+        await sleep(1)
+        oddNumbers.push(currentOddNumber)
+      }
+    })(),
+  ])
+
+  expect(evenNumbers).toMatchInlineSnapshot(`
+    [
+      0,
+      2,
+      4,
+      6,
+      8,
+    ]
+  `)
+  expect(oddNumbers).toMatchInlineSnapshot(`
+    [
+      1,
+      3,
+      5,
+      7,
+      9,
+    ]
+  `)
 })
